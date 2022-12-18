ec0e2ee2a266d26752b223390518794acc33c5e7
NWCHASER <112234001+NWCHASER@users.noreply.github.com>
2022-12-18 13:22:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default size
+cd openwrt
+rm /openwrt/blob/master/target/linux/ipq806x/files-5.15/arch/arm/boot/dts/qcom-ipq8064-r7500.dts
+mv /files/qcom-ipq8064-r7500.dts /openwrt/blob/master/target/linux/ipq806x/files-5.15/arch/arm/boot/dts/
