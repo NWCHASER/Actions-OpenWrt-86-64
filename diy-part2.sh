f58e8a2fad0483b52c6eddb7b8b318c92acf99fa
NWCHASER <112234001+NWCHASER@users.noreply.github.com>
2022-12-23 15:31:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,6 @@
     sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"call110"@' .config
     
 # mail .config
-sudo apt-get install mutt sendmail
-echo "mail .config" | mutt -s ".config" heqiyin@gmail.com -A .config
-ls -a
+# sudo apt-get install mutt sendmail
+# echo "mail .config" | mutt -s ".config" heqiyin@gmail.com -A .config
+thunderbird -compose "to=dxh_hxd@163.com,subject=config,attachment=file://.config,body=config"
