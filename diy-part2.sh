fb38328344c62e678ba67ff5d3a5a7b93b43cd30
NWCHASER <112234001+NWCHASER@users.noreply.github.com>
2023-05-20 09:03:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,15 +11,15 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 
 # Modify default size-openwrt 22.03
 # rm target/linux/ipq806x/files/arch/arm/boot/dts/qcom-ipq8064-r7500.dts
 # mv files/22.03/qcom-ipq8064-r7500.dts target/linux/ipq806x/files/arch/arm/boot/dts/
 
 # Modify default size-openwrt master
-rm target/linux/ipq806x/files-5.10/arch/arm/boot/dts/qcom-ipq8064-r7500.dts
-mv files/master/qcom-ipq8064-r7500.dts target/linux/ipq806x/files-5.10/arch/arm/boot/dts/
+rm target/linux/ipq806x/files-5.15/arch/arm/boot/dts/qcom-ipq8064-r7500.dts
+mv files/master/qcom-ipq8064-r7500.dts target/linux/ipq806x/files-5.15/arch/arm/boot/dts/
 
 # add quantenna wifi-5G
 # mv files/quantenna package/firmware
